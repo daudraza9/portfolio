8a4b0430cf6381209a9436394ba7c1dedb04ce83
Daud Raza <daud.raza@conovoinc.com>
2022-06-03 11:07:46 +0500

    added-403-error-file

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -21,7 +21,7 @@ class FrontController extends Controller
     public function aboutUs(Request $request,$slug)
     {
         $people = People::where('slug',$slug)->get();
-      
+
         $skills = Skill::where('person_id',$people[0]->id)->get();
         $education = Education::where('people_id',$people[0]->id)->get();
         $experience = Experience::where('people_id',$people[0]->id)->get();
@@ -47,7 +47,18 @@ class FrontController extends Controller
     public function person(Request $request,$slug)
     {
        $people = People::where('slug',$slug)->get();
+       if (!isset($people[0]))
+       {
+//           dd('present');
+           return view('landing.403');
+
+       }
+       else
+       {
+//           dd('not');
+           return view('landing.person',compact('people'));
+       }
     //    dd($people[0]);
-        return view('landing.person',compact('people'));
+
     }
 }
--- resources/views/landing/403.blade.php
+++ resources/views/landing/403.blade.php
@@ -11,8 +11,8 @@
             <p> Click here to visit <a href="#"> Truesols </a> for more information about this suspension. </p>
     </div>
     <div style="text-align: center;height:90vh; display: block;">
-        <img src="{{asset('admin-assets/images/icon/error-404.png')}}" alt="" style="width:auto;height:100%;">
-        
+        <img src="{{asset('front-assets/img/error-404.png')}}" alt="" style="width:auto;height:100%;">
+
     </div>
 </body>
 </html>
